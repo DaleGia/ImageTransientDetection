608b06c483e777e87cf37004d561dc4f3d410a94
DaleGia <d.giancono@arborcarbon.com.au>
2025-05-05 16:04:06 +0800

    Adding zoom windows. Fixing resizing

--- ImagePreviewWindow.hpp
+++ ImagePreviewWindow.hpp
@@ -9,13 +9,25 @@
 class ImagePreviewWindow
 {
 public:
-    ImagePreviewWindow(std::string windowName) : name(windowName) {
-                                                 };
+    ImagePreviewWindow(std::string windowName) : name(windowName)
+    {
+        cv::namedWindow(name, cv::WINDOW_NORMAL || cv::WindowFlags::WINDOW_KEEPRATIO);
+        cv::setMouseCallback(name, ImagePreviewWindow::onMouseWheel, this);
+    };
+
+    ImagePreviewWindow(std::string windowName, uint32_t width, uint32_t height) : name(windowName), width(width), height(height)
+    {
+        cv::namedWindow(name, cv::WINDOW_NORMAL || cv::WindowFlags::WINDOW_KEEPRATIO);
+        cv::setMouseCallback(name, ImagePreviewWindow::onMouseWheel, this);
+        this->setSize(width, height);
+    };
 
     void setSize(uint16_t width, uint16_t height)
     {
         cv::setWindowProperty(this->name, cv::WindowPropertyFlags::WND_PROP_ASPECT_RATIO, width / height);
         cv::resizeWindow(this->name, width, height);
+        this->width = width;
+        this->height = height;
     }
 
     void setImage(cv::Mat &image)
@@ -53,9 +65,13 @@ public:
                 this->currentImage,
                 CV_8UC1);
         }
-
-        cv::imshow(this->name, currentImage);
-        cv::waitKey(1);
+        cv::imshow(this->name, this->currentImage);
+        if (this->width != 0 && this->height != 0)
+        {
+            cv::setWindowProperty(this->name, cv::WindowPropertyFlags::WND_PROP_ASPECT_RATIO, this->width / this->height);
+            cv::resizeWindow(this->name, this->width, this->height);
+        }
+        cv::waitKey(50);
     };
 
     void setImageStreched(cv::Mat &image, double percent)
@@ -121,12 +137,135 @@ public:
 
         cv::setWindowTitle(this->name, title);
         cv::imshow(this->name, this->currentImage);
-        cv::waitKey(1);
+        if (this->width != 0 && this->height != 0)
+        {
+            cv::setWindowProperty(this->name, cv::WindowPropertyFlags::WND_PROP_ASPECT_RATIO, this->width / this->height);
+            cv::resizeWindow(this->name, this->width, this->height);
+        }
+        cv::waitKey(50);
     }
 
 private:
     cv::Mat currentImage;
     std::string name;
+
+    bool zoomEnabled = false;
+    bool statEnabled = false;
+    int xzoom = 0;
+    int yzoom = 0;
+    std::string zoomName;
+    std::string statName;
+    uint32_t width = 0;
+    uint32_t height = 0;
+    void showZoomed(void)
+    {
+        // Calculate the top-left corner of the ROI, ensuring it's within the image boundaries
+        int x1 = std::max(0, this->xzoom - 50);
+        int y1 = std::max(0, this->yzoom - 50);
+
+        // Calculate the bottom-right corner, ensuring it's within the image boundaries
+        int x2 = std::min(this->currentImage.cols - 1, this->xzoom + 50);
+        int y2 = std::min(this->currentImage.rows - 1, this->yzoom + 50);
+
+        // Create a rectangle for the ROI
+        cv::Rect roi(x1, y1, x2 - x1 + 1, y2 - y1 + 1);
+
+        // Extract the ROI from the image
+        cv::Mat cropped_img = this->currentImage(roi).clone();
+        cv::resize(cropped_img, cropped_img, cv::Size(400, 400));
+        cv::imshow(this->zoomName, cropped_img);
+    };
+
+    void showZoomedStats(void)
+    {
+        cv::Scalar std;
+        cv::Scalar mean;
+        double statsMin;
+        double statsMax;
+        bool isColour;
+
+        // Calculate the top-left corner of the ROI, ensuring it's within the image boundaries
+        int x1 = std::max(0, this->xzoom - 25);
+        int y1 = std::max(0, this->yzoom - 25);
+
+        // Calculate the bottom-right corner, ensuring it's within the image boundaries
+        int x2 = std::min(this->currentImage.cols - 1, this->xzoom + 25);
+        int y2 = std::min(this->currentImage.rows - 1, this->yzoom + 25);
+
+        // Create a rectangle for the ROI
+        cv::Rect roi(x1, y1, x2 - x1 + 1, y2 - y1 + 1);
+
+        // Extract the ROI from the image
+        cv::Mat cropped_img = this->currentImage(roi).clone();
+
+        cv::meanStdDev(cropped_img, mean, std);
+        cv::minMaxLoc(cropped_img, &statsMin, &statsMax);
+
+        if (true == isColour)
+        {
+            cropped_img.convertTo(
+                cropped_img,
+                CV_8UC3,
+                255.0 / (statsMax - statsMin), -statsMin * 255.0 / (statsMax - statsMin));
+        }
+        else
+        {
+            cropped_img.convertTo(
+                cropped_img,
+                CV_8UC1,
+                255.0 / (statsMax - statsMin), -statsMin * 255.0 / (statsMax - statsMin));
+        }
+        cv::resize(cropped_img, cropped_img, cv::Size(400, 400), 0, 0, cv::INTER_NEAREST);
+        std::string title = "Min: " + std::to_string((int)statsMin) + " Max: " + std::to_string((int)statsMax) + " Mean: " + std::to_string((int)mean[0]) + " STD: " + std::to_string((int)std[0]);
+        cv::setWindowTitle(this->statName, title);
+        cv::imshow(this->statName, cropped_img);
+    }
+
+    static void onMouseWheel(int event, int x, int y, int flags, void *userdata)
+    {
+        ImagePreviewWindow *window = (ImagePreviewWindow *)userdata;
+
+        if (event == cv::EVENT_MOUSEMOVE)
+        {
+            window->xzoom = x;
+            window->yzoom = y;
+        }
+
+        else if (event == cv::EVENT_LBUTTONDOWN)
+        {
+            if (false == window->zoomEnabled)
+            {
+                window->zoomEnabled = true;
+            }
+            else
+            {
+                window->zoomEnabled = false;
+                cv::destroyWindow(window->zoomName);
+            }
+        }
+        else if (event == cv::EVENT_RBUTTONDOWN)
+        {
+            if (false == window->statEnabled)
+            {
+                window->statEnabled = true;
+            }
+            else
+            {
+                window->statEnabled = false;
+                cv::destroyWindow(window->statName);
+            }
+        }
+
+        if (true == window->zoomEnabled)
+        {
+            window->showZoomed();
+        }
+
+        if (true == window->statEnabled)
+        {
+            window->showZoomedStats();
+        }
+    };
 };
 
 #endif // IMAGEPREVIEWWINDOW_H
